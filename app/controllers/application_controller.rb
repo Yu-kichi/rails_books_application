abbda9e87a8462d12d2d99a303466429c33f6d26
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2020-08-16 23:14:53 +0900

    modify view
    見た目の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 
 class ApplicationController < ActionController::Base
   #add_flash_types :success, :info, :warning, :danger
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index,:show]
   # deviseコントローラーにストロングパラメータを追加する          
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     # サインアップ時にnameのストロングパラメータを追加
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    # アカウント編集の時にnameとprofileのストロングパラメータを追加
+    # アカウント編集の時にnameのストロングパラメータを追加
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
